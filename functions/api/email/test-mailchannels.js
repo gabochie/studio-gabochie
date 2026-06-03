1e4ef7541acc5faf4c177589de2cf8a9c914dc5d
gabochie <42058704+gabochie@users.noreply.github.com>
2026-06-03 07:16:20 +0000

    fix: debug MailChannels 401 by capturing response body

--- functions/api/email/test-mailchannels.js
+++ functions/api/email/test-mailchannels.js
@@ -1,24 +1,27 @@
 import { sendMailChannels } from './_send-mailchannels.js';
 
 export async function onRequest(context) {
-  var { env } = context;
+  var { env, request } = context;
   var hasKey = !!env.DKIM_PRIVATE_KEY;
   var keyStart = hasKey ? env.DKIM_PRIVATE_KEY.substring(0, 27) + '...' : '(not set)';
   try {
     var res = await sendMailChannels(env, env.NOTIFY_EMAIL || 'gid@gideonabochie.com', 'Test', 'MailChannels DKIM Test', '<p>If you see this, MailChannels + DKIM is working.</p>');
+    var body = '';
+    try { body = await res.text(); } catch (_) {}
     return new Response(JSON.stringify({
       dkim_key_set: hasKey,
       dkim_key_preview: keyStart,
       mailchannels_status: res.status,
       mailchannels_ok: res.ok,
-      message: hasKey ? 'DKIM key found. MailChannels returned ' + res.status : 'DKIM_PRIVATE_KEY env var is NOT set.'
+      mailchannels_body: body.substring(0, 500),
+      message: 'DKIM key ' + (hasKey ? 'found' : 'NOT set') + '. MailChannels returned ' + res.status
     }), { headers: { 'Content-Type': 'application/json' } });
   } catch (err) {
     return new Response(JSON.stringify({
       dkim_key_set: hasKey,
       dkim_key_preview: keyStart,
       error: err.message,
-      message: 'DKIM key check completed. MailChannels call failed.'
+      message: 'MailChannels call threw exception: ' + err.message
     }), { headers: { 'Content-Type': 'application/json' } });
   }
 }
