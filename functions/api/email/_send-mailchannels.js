66b5b273472f4e99e8634a9ae6306800b7b20685
gabochie <42058704+gabochie@users.noreply.github.com>
2026-06-03 07:14:26 +0000

    fix: remove unsupported dkim.privateKey from MailChannels API call

--- functions/api/email/_send-mailchannels.js
+++ functions/api/email/_send-mailchannels.js
@@ -11,14 +11,6 @@ export async function sendMailChannels(env, toEmail, toName, subject, htmlConten
     subject: subject,
     content: [{ type: 'text/html', value: htmlContent }]
   };
-  var dkimKey = env.DKIM_PRIVATE_KEY;
-  if (dkimKey) {
-    payload.personalizations[0].dkim = {
-      domain: env.DKIM_DOMAIN || 'gideonabochie.org',
-      selector: env.DKIM_SELECTOR || 'newsletter',
-      privateKey: dkimKey
-    };
-  }
   var res = await fetch(MC_ENDPOINT, {
     method: 'POST',
     headers: { 'Content-Type': 'application/json' },
