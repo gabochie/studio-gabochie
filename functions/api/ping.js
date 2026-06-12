1f85b72c56fbfeafe93f81a8f214c62127f34a05
gabochie <42058704+gabochie@users.noreply.github.com>
2026-06-12 10:13:41 +0000

    cleanup: remove test files, unused whatsapp/ subdir, finalize flat endpoint structure

--- functions/api/ping.js
+++
@@ -1,5 +0,0 @@
-export async function onRequest(context) {
-  return new Response(JSON.stringify({ status: 'ok', message: 'New root API file works!' }), {
-    headers: { 'Content-Type': 'application/json', 'Access-Control-Allow-Origin': '*' }
-  });
-}
